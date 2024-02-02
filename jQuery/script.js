9910e12cfe4544cdf588266cb8b9912b9540ff21
Rohan Kadam <rohankadam0148@gmail.com>
2024-02-02 20:57:02 +0530

    Manipulating and introducing jQuery

---
+++ jQuery/script.js
@@ -0,0 +1,4 @@
+$("button").css("border", "solid 2px black"); // by default selects all the buttons needs to specify extra details to select individual buttons
+console.log($("h1").css("font-size"));
+$("h1").addClass("big-title");
+$("h1").css("color", "black");
