feb3b71decbc8ca0ec4b90128637ec93b2ad8163
Rohan Kadam <rohankadam0148@gmail.com>
2024-02-26 20:23:55 +0530

    jQuery Practice

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,4 +1,28 @@
 $("button").css("border", "solid 2px black"); // by default selects all the buttons needs to specify extra details to select individual buttons
 console.log($("h1").css("font-size"));
-$("h1").addClass("big-title");
+
+$("h1").toggleClass("big-title");
+
 $("h1").css("color", "black");
+
+$("h1").click(function () {
+  $("h1").css("color", "red");
+});
+
+$("button").click(function () {
+  $("p").css("color", "darkblue");
+  $("p").css("fontSize", "30px");
+});
+
+$(document).keypress(function (event) {
+  console.log(event.key);
+  $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function () {
+  $("h1").css("color", "pink");
+});
+
+$("button").on("click", function () {
+  $("h1").slideToggle();
+});
